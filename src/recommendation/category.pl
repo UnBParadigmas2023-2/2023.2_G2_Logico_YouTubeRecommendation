ab3673ac4456f6448f5cb82fe65e03b6bde7fdc7
ciceroff <cicero.barrozo13@gmail.com>
2023-10-25 20:08:16 -0300

    work in progress category filter

--- src/recommendation/category.pl
+++ src/recommendation/category.pl
@@ -1,38 +1,42 @@
-:- use_module(library(dcg/basics)).
-:- use_module(library(format)).
-:- use_module(library(system)).
+use_module(library(readutil)).
+:- [src/tmp/data].
+:- (dynamic category/1, video/5).
+:- debug.
 
-:- use_module(library(http/json)).
+menu_general_recommend :-
+    new(Dialog, dialog('Recomendacao Geral')),
+    send(Dialog, append, new(Titulo, text('Recomendação por categoria'))), 
+     send_list(Dialog,
+              append,
+              
+              [ new(N1, menu(category, cycle)),
+                button(cancel, message(Dialog, destroy)),
+                button(enter,
+                       and(message(@prolog,
+                                   filter_by_category,
+                                   N1?selection),
+                           message(Dialog, destroy)))
+              ]),
+    forall(category(C), send_list(N1, append, C)),
+    send(Dialog, open).
 
-% Regra para carregar os dados do JSON
-carregar_json(Dados) :-
-    json_read_dict('categories.json', Dados).
+filter_by_category(Categoria) :-
+     new(D, dialog('Vídeos Recomendados')),
+    findall(Video,
+            video(category,
+                  _,
+                  _,
+                  _,
+                  _,
+                  _),
+            Titles),
+    append_text_dialog(Titles, D),
+    send(D, open).
 
-% Exemplo de dados carregados do JSON
-% (Certifique-se de substituir isso pelos dados reais do JSON)
-dados_json([
-    _{ id: "1", title: "Film & Animation" },
-    _{ id: "2", title: "Autos & Vehicles" },
-    _{ id: "3", title: "Music" },
-    % Adicione mais categorias conforme necessário
-]).
+append_text_dialog([], D).
+append_text_dialog([A|B], D) :-
+    send(D, append, text(A)),
+    append_text_dialog(B, D).
 
-% Exemplo de dados de vídeos
-% (Certifique-se de substituir isso pelos dados reais)
-dados_de_videos([
-    _{ video_id: "abc123", title: "Video 1", category_id: "1" },
-    _{ video_id: "def456", title: "Video 2", category_id: "2" },
-    _{ video_id: "ghi789", title: "Video 3", category_id: "1" },
-    % Adicione mais vídeos e categorias conforme necessário
-]).
-
-% Regra para recomendar vídeos por categoria
-recomendar_videos_por_categoria(Categoria, VideosRecomendados) :-
-    dados_json(Categorias), % Carregar as categorias do JSON
-    dados_de_videos(Videos), % Carregar os dados dos vídeos
-
-    % Encontrar vídeos com base na categoria
-    findall(Video, (member(Cat, Categorias), Cat.title = Categoria, member(Video, Videos), Video.category_id = Cat.id), VideosRecomendados).
-
-% Exemplo de uso: Recomendar vídeos na categoria "Film & Animation"
-?- recomendar_videos_por_categoria("Film & Animation", VideosRecomendados).
+views():-
+    menu_general_recommend.
