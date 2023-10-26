0421093489f9ece4a3f3c6dfaf6abe8a75bb54ad
ciceroff <cicero.barrozo13@gmail.com>
2023-10-25 21:30:55 -0300

    corrigindo sintaxe
    Co-authored-by: joao-henrique10 <joao-henrique10@users.noreply.github.com>
    Co-authored-by: Gabriel Moretti <bielmoreti@gmail.com>
    Co-authored-by: Adne Moretti  <AdneMoretti@users.noreply.github.com>
    Co-authored-by: Lucas Lopes <luclopesr@users.noreply.github.com>

--- src/recommendation/date.pl
+++ src/recommendation/date.pl
@@ -1,31 +1,27 @@
+:- use_module(library(pce)).
 :- use_module(library(dcg/basics)).
 :- use_module(library(format)).
 :- use_module(library(system)).
-
-video("video1", "Título 1", "Descrição 1", "2023-10-23T14:30:00Z", "Categoria 1", 1000).
-video("video2", "Título 2", "Descrição 2", "2023-10-22T15:45:00Z", "Categoria 2", 1500).
-video("video3", "Título 3", "Descrição 3", "2023-10-15T12:15:00Z", "Categoria 1", 2000).
-video("video4", "Título 4", "Descrição 4", "2023-09-30T18:20:00Z", "Categoria 3", 800).
 
 % Obtém a data e hora atuais no formato desejado
 get_current_date(DataAtual) :-
     get_time(TimeStamp),
     stamp_date_time(TimeStamp, DateTime, 'UTC'),
-    format_time(atom(DataAtual, '%FT%T%z', DateTime).
+    format_time(atom(DataAtual, '%FT%T%z', DateTime)).
 
 % Subtrai uma quantidade específica de horas de uma data e hora no formato desejado
 subtrair_horas(DataHora, HorasASubtrair, NovaDataHora) :-
     parse_time(DataHora, DataTime),
     SegundosASubtrair is HorasASubtrair * 3600, % Converte horas em segundos
     DataTime - SegundosASubtrair,
-    format_time(atom(NovaDataHora, '%FT%T%z', DataTime).
+    format_time(atom(NovaDataHora, '%FT%T%z', DataTime)).
 
 % Subtrai uma quantidade específica de dias de uma data e hora no formato desejado
 subtrair_dias(DataHora, DiasASubtrair, NovaDataHora) :-
     parse_time(DataHora, DataTime),
     SegundosASubtrair is DiasASubtrair * 86400, % Converte dias em segundos
     DataTime - SegundosASubtrair,
-    format_time(atom(NovaDataHora, '%FT%T%z', DataTime).
+    format_time(atom(NovaDataHora, '%FT%T%z', DataTime)).
 
 % Regra para recomendar vídeos com base na data de upload
 recomendar_videos(Opcao, VideosRecomendados) :-
@@ -58,6 +54,7 @@ recomendar_videos_por_opcao("Este ano", DataAtual, VideosRecomendados) :-
 % Consulta de recomendações usando caixa de diálogo
 consultar_recomendacoes :-
     new(Dlg, dialog('Recomendação de Vídeos')),
+    send(Dlg, size, size(800, 800)), 
     send(Dlg, append, new(Menu, menu(opcao, cycle))),
     opcoes_menu(OpcoesMenu),
     forall(member(Opcao, OpcoesMenu), send(Menu, append, Opcao)),
