aefa18938b6e43342166307530c6f66ce10c5ea5
Guilherme Puida <guilherme@puida.xyz>
2023-10-27 10:09:37 -0300

    Reestruturando organização do problema
    Co-authored-by: joao-henrique10 <joao-henrique10@users.noreply.github.com>
    Co-authored-by: Lucas Lopes <luclopesr@users.noreply.github.com>
    Co-authored-by: Caio Vitor Carneiro de Oliveira <caiozim112@users.noreply.github.com>
    Co-authored-by: Gabriel Moretti <MorettiGS@users.noreply.github.com>
    Co-authored-by: Adne Moretti <AdneMoretti@users.noreply.github.com>
    Co-authored-by: Gabriel Costa <gabriel.costa513@gmail.com
    Co-Authored-by: Cícero Fernandes <65192073+ciceroff@users.noreply.github.com>

--- src/recommendation/date.pl
+++ src/recommendation/date.pl
@@ -21,33 +21,65 @@ data_por_opcao(semestre, Data) :-
   meses_em_segundos(6, Segundos),
   Data is (DataAtual - Segundos).
 
+data_por_opcao(sempre, Resultado) :-
+  Resultado is 0.
+
+proporcao_por_opcao(todos, Low, High) :-
+  Low is 0, High is 1.
+
+proporcao_por_opcao(baixa, Low, High) :-
+  Low is 0, High is 0.1.
+
+proporcao_por_opcao(media, Low, High) :-
+  Low is 0.1, High is 0.2.
+
+proporcao_por_opcao(alta, Low, High) :-
+  Low is 0.2, High is 0.3.
+
+format_video(Category, Channel, Name, Result) :-
+  format(atom(Result), '~w | ~w | ~w', [Category, Channel, Name]).
+
 recomendar_por_data :-
   new(Dialog, dialog('Recomendar por data')),
   send(Dialog, size, size(900, 900)),
   send_list(Dialog, append,
             [ new(D, menu(data, cycle)),
-              button(cancelar, message(Dialog, destroy)),
+              new(C, menu(categoria, cycle)),
+              new(P, menu(proporcao_de_likes, cycle)),
+              button(fechar, message(Dialog, destroy)),
               button(pesquisar, and(message(@prolog,
                                             mostrar_recomendacoes,
-                                            D?selection),
-                                    message(Dialog, destroy)))
+                                            D?selection,
+                                            C?selection,
+                                            P?selection)))
             ]),
-  send_list(D, append, [duas_semanas, mes, semestre]),
+  send_list(D, append, [sempre, duas_semanas, mes, semestre]),
+  send_list(P, append, [todos, baixa, media, alta]),
+  send(C, append, todas),
+  forall(category(CC), send(C, append, CC)),
   send(Dialog, default_button, pesquisar),
   send(Dialog, open).
 
-mostrar_recomendacoes(D) :-
-  data_por_opcao(D, DataLimite),
+mostrar_recomendacoes(D, C, P) :-
   new(Dialog, dialog('Recomendações')),
-  send(Dialog, size, size(900, 900)),
+  send(Dialog, size, size(1000, 900)),
   send_list(Dialog, append,
-            [ new(LB, list_browser),
-              button(fechar, message(Dialog, destroy))
+            [ button(fechar, message(Dialog, destroy)),
+              new(LB, list_browser)
             ]),
-  send(Dialog, default_button, fechar),
-  send(LB, size, size(200, 200)),
+  data_por_opcao(D, DataLimite),
+  proporcao_por_opcao(P, Low, High),
+  send(LB, size, size(150, 50)),
   forall(
-    (video(_, _, N, Dd, _), Dd >= DataLimite),
-    send(LB, append, N)
+    (
+      video(Cat, Chan, Nome, Data, Lpv),
+      Data >= DataLimite,
+      (C = todas; Cat = C),
+      Lpv >= Low,
+      Lpv =< High,
+      format_video(Cat, Chan, Nome, R)
+    ),
+    send(LB, append, R)
   ),
+  send(Dialog, default_button, fechar),
   send(Dialog, open).
